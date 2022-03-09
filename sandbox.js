2c630494a7065bd91375cf7e01947db126174e14
eelisavolainen <eeli.savolainen@metropolia.fi>
2022-03-09 19:13:14 +0200

    Added some javascript, first 15min of the video

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2022;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+// points = 50; cannot do this, can't revalue the const variable
+console.log(points);
+
+var score = 75;
+console.log(score);
