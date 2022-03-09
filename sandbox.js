0a77424096427fadf55e810de2150d6433e9a14e
eelisavolainen <eeli.savolainen@metropolia.fi>
2022-03-09 23:03:39 +0200

    Video time stamp 1:01:54

--- sandbox.js
+++ sandbox.js
@@ -78,7 +78,7 @@ console.log(10/2);
 
 // console.log(result);
 
-let likes = 10;
+// let likes = 10;
 
 // likes = likes + 1;
 // likes++;
@@ -88,9 +88,67 @@ let likes = 10;
 // likes *=2;
 // likes /=2;
 
-console.log(likes);
+//console.log(likes);
 
 // NaN - not a number
 
-console.log(5/'hello');
-console.log(5*'hello');
+// console.log(5/'hello');
+// console.log(5*'hello');
+
+//let result = 'the blog has ' + likes + ' likes';
+//console.log(result);
+
+
+//TEMPLATE STRINGS
+
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+
+// template string way
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+
+
+// creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>${author}</p>
+    <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
+
+
+// ARRAYS
+
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let ages = [20,25,30,35];
+// console.log(ages[2]);
+
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// ARRAY METHODS
+
+// let result = ninjas.join(',');
+
+// let result = ninjas.indexOf('chun-li');
+
+// let result = ninjas.concat(['ken','crystal']);
+
+let result =ninjas.push('ken');
+result = ninjas.pop();
+console.log(result);
